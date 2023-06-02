196c04d680617be207c03c1af3e9addefe0aa834
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2023-06-01 23:20:25 -0300

    melhorias visuais

--- app.py
+++ app.py
@@ -16,26 +16,39 @@ st.set_page_config(
     }
 )
 
-#ordem das commodites no array: ouro, prata, platina, cobre, pretoleo cru, gas natural e café. 
-lista_commodities = ['GC=F', 'SI=F', 'PL=F', 'HG=F', 'CL=F', 'NG=F', 'KC=F']
+st.title("DASHBOARD DAS :blue[COMMODITIES]")
 
-st.title("DASHBOARD DAS COMMODITIES")
+#       ordem das commodites no array: ouro, prata, platina, cobre, pretoleo cru, gas natural e café. 
+lista_commodities = ['GC=F', 'SI=F', 'PL=F', 'HG=F', 'CL=F', 'NG=F', 'KC=F', 'CB=F', 'CT=F']
 
-#recebendo a data do input
-data_inicio=st.date_input("Escolha a data inicial:", datetime.date(2020, 1, 1))
+#       recebendo a data do input
+data_inicio=st.date_input("Escolha a data inicial:", datetime.date(2023, 1, 1))
 data_fim=st.date_input("Escolha a data final:", date.today())
+
+#       fazendo download dos valores via yfinance
 commodities_tudo=yf.download(lista_commodities, start=data_inicio, end=data_fim)['Adj Close']
 
-tab1, tab2, tab3 = st.tabs(["📈 Gráfico", " 💹 Correlação", "Correlação selecionada"])
+#       renomeando as commodities
+r_pd_commodities_tudo=pd.DataFrame(commodities_tudo.rename(columns={'CL=F':'Petroleo Cru', 'GC=F':'Ouro', 'HG=F':'Cobre', 'KC=F':'Café', 'NG=F':'Gás natural', 
+                                                                    'PL=F':'Platina', 'SI=F':'Prata', 'CT=F':'Algodão', 'CB=F': 'Açúcar'}))
+#       tirando a hora '00:00:00' da coluna 'Date'
+r_pd_commodities_tudo.index=r_pd_commodities_tudo.index.date
+
+tab1, tab2, tab3 = st.tabs(["📈 Gráfico", " 🙅‍♂️ Correlação", "✅ Correlação selecionada"])
 
 with tab1:
-    st.header("GRÁFICO")
+    st.header("LISTAGEM")
+    
+    r_pd_commodities_tudo
 
-    #plotando
-    st.line_chart(commodities_tudo)
+    st.divider()
+
+    st.header("GRÁFICO")
+    #       plotando
+    st.line_chart(r_pd_commodities_tudo)
 
     with st.expander("Ver explicação"):
-        st.write("O gráfico acima mostra a variação de preço (em U$), das seguintes :blue[commodities]: ouro, prata, platina, cobre, pretoleo cru, gas natural e café.")
+        st.write("O gráfico acima mostra a variação de preço (em U$), das :blue[COMMODITIES].")
         #imagem=Image.open('/home/jtentis/Documentos/projetos/commodities/imagens/commodities.png')
         #st.image(imagem, width=300)
         
@@ -43,39 +56,44 @@ with tab1:
 with tab2:
     st.header("CORRELAÇÃO")
 
-    #mostrando o dataframe da correlação
-    st.dataframe(commodities_tudo.corr())
+    #       mostrando o dataframe da correlação
+    st.dataframe(r_pd_commodities_tudo.corr())
 
     with st.expander("Ver explicação"):
-        st.write("O dataframe acima mostra a correlação das :blue[commodities].")
+        st.write("O DataFrame acima mostra a correlação das :blue[COMMODITIES].")
 
 with tab3:
     st.header("CORRELAÇÃO SELECIONADA")
-    st.subheader("Selecione pelo menos 2 :blue[commodities] para correlação!")
+    st.subheader("Selecione pelo menos 2 :blue[COMMODITIES] para correlação!")
 
     selOuro = st.checkbox('Ouro')
     selPrata = st.checkbox('Prata')
     selPlatina = st.checkbox('Platina')
 
     if selOuro and selPrata:
-        commodities_corr=['GC=F', 'SI=F']
-        commodities_corr=yf.download(commodities_corr, start=data_inicio, end=data_fim)['Adj Close']
-        st.dataframe(commodities_corr.corr()) 
+        lista_sel=['GC=F', 'SI=F']
+        commodities_corr=yf.download(lista_sel, start=data_inicio, end=data_fim)['Adj Close']
+        r_commodities_corr=pd.DataFrame(commodities_corr.rename(columns={'GC=F': 'Ouro', 'SI=F':'Prata'}))
+        st.dataframe(r_commodities_corr.corr())
 
     elif selOuro and selPlatina:
-        commodities_corr2=['GC=F', 'PL=F']
-        commodities_corr2=yf.download(commodities_corr2, start=data_inicio, end=data_fim)['Adj Close']
-        st.dataframe(commodities_corr2.corr())
+        lista_sel2=['GC=F', 'PL=F']
+        commodities_corr=yf.download(lista_sel2, start=data_inicio, end=data_fim)['Adj Close']
+        r_commodities_corr2=pd.DataFrame(commodities_corr.rename(columns={'GC=F': 'Ouro', 'PL=F':'Platina'}))
+        st.dataframe(r_commodities_corr2.corr())
+
 
     elif selPrata and selPlatina:
-        commodities_corr3=['SI=F', 'PL=F']
-        commodities_corr3=yf.download(commodities_corr3, start=data_inicio, end=data_fim)['Adj Close']
-        st.dataframe(commodities_corr3.corr())
+        lista_sel3=['SI=F', 'PL=F']
+        commodities_corr=yf.download(lista_sel3, start=data_inicio, end=data_fim)['Adj Close']
+        r_commodities_corr3=pd.DataFrame(commodities_corr.rename(columns={'SI=F': 'Prata', 'PL=F':'Platina'}))
+        st.dataframe(r_commodities_corr3.corr())
     
     elif selPrata and selPlatina and selOuro:
-        commodities_corr4=['GC=F', 'SI=F', 'PL=F']
-        commodities_corr4=yf.download(commodities_corr4, start=data_inicio, end=data_fim)['Adj Close']
-        st.dataframe(commodities_corr4.corr())
+        lista_sel4=['SI=F', 'PL=F', 'GC=F']
+        commodities_corr=yf.download(lista_sel4, start=data_inicio, end=data_fim)['Adj Close']
+        r_commodities_corr4=pd.DataFrame(commodities_corr.rename(columns={'SI=F': 'Prata', 'PL=F':'Platina', 'GC=F':'Ouro'}))
+        st.dataframe(r_commodities_corr4.corr())
 
     else:
         st.divider()
