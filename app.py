1806235530c405e51a7599bfa29463dc9d7b1654
Joao Tentis <101594270+jtentis@users.noreply.github.com>
2023-06-23 22:25:30 -0300

    nova feature de ativos

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import datetime
 import matplotlib.pyplot as plt
-import matplotlib
 from datetime import date, timedelta
 from PIL import Image
-import time
+# import matplotlib
+# import time
+# import datetime
 
 st.set_page_config(
     page_title="Página inicial / Commodities",
@@ -25,6 +25,8 @@ st.title("DASHBOARD DAS :orange[COMMODITIES]")
 
 #       ordem das commodites no array: ouro, prata, platina, cobre, pretoleo cru, gas natural e café. 
 lista_commodities = ['GC=F', 'SI=F', 'PL=F', 'HG=F', 'CL=F', 'NG=F', 'KC=F', 'SB=F', 'CT=F', 'CC=F', 'ZS=F', 'ZC=F', 'LE=F', 'KE=F']
+lista_ativos = ["^BVSP","GGBR4.SA", "CMIG4.SA", "PETR4.SA", "PETR3.SA", "VIVT3.SA", "VALE3.SA", "BBAS3.SA", "CPLE6.SA", "JBSS3.SA", 
+                 "TRPL4.SA", "SANB11.SA", "BBSE3.SA", "EGIE3.SA", "B3SA3.SA", "TAEE11.SA", "ITSA4.SA", "VBBR3.SA", "GOAU4.SA", "CSNA3.SA", "CMIG3.SA", "CPFE3.SA"]
 
 logo=Image.open("imagens/OBInvestLogo.png")
 
@@ -50,6 +52,7 @@ with st.sidebar:
 
 #       fazendo download dos valores via yfinance
 commodities_tudo=yf.download(lista_commodities, start=data_inicio, end=data_fim)['Adj Close']
+ativos_tudo=yf.download(lista_ativos, start=data_inicio, end=data_fim)['Adj Close']
 
 tickers = yf.Tickers(lista_commodities)
 week=data_fim-timedelta(days=1)
@@ -65,12 +68,10 @@ dif_percentual={'CL=F':'Petroleo Cru', 'GC=F':'Ouro', 'HG=F':'Cobre', 'KC=F':'Ca
 r_tickers_hist=pd.DataFrame(tickers_hist.rename(columns={'CL=F':'Petroleo Cru', 'GC=F':'Ouro', 'HG=F':'Cobre', 'KC=F':'Café', 'NG=F':'Gás natural', 
                                                                     'PL=F':'Platina', 'SI=F':'Prata', 'CT=F':'Algodão', 'SB=F': 'Açúcar', 'CC=F':'Cacau', 
                                                                     'ZS=F': 'Soja', 'ZC=F':'Milho', 'LE=F':'Gado', 'KE=F':'Trigo'}))
+r_pd_ativos=pd.DataFrame(ativos_tudo)
+
 #       tirando a hora '00:00:00' da coluna 'Date'
-r_pd_commodities_tudo2=r_pd_commodities_tudo
 # r_pd_commodities_tudo.index=r_pd_commodities_tudo.index.date
-# r_tickers_hist.index=r_tickers_hist.index.date
-# r_pd_commodities_tudo['Date'] = pd.to_datetime(r_pd_commodities_tudo['Date']).dt.date
-# r_pd_commodities_tudo['Date'] = r_pd_commodities_tudo['Date'].str.split(' ').str[0]
 
 tab1, tab2, tab3, tab4 = st.tabs(["📈 Gráfico Geral", "🗓️ Report Semanal", " 🙅‍♂️ Correlação Geral", "✅ Correlação Selecionada"])
 
@@ -97,7 +98,7 @@ with tab1:
 
 
     with st.expander("Ver explicação"):
-        st.write("O gráfico acima mostra a variação de preço (em :green[U$]), das :orange[COMMODITIES].")
+        st.write("Acima, uma tabela e um gráfico que mostram a variação de preço (em :green[U$]), das :orange[COMMODITIES].")
     
     st.download_button("Baixar Tabela", 
                        r_pd_commodities_tudo.to_csv(),
@@ -111,11 +112,11 @@ with tab2:
     st.write("")
     tickers_report=r_tickers_hist.stack(level=1).rename_axis(['Data', 'Tickers']).reset_index(level=1)
     report_semanal=tickers_report.drop(['Dividends','Volume','Stock Splits'], axis=1)
-    report_semanal['Resultado']=(report_semanal['Close'] - report_semanal['Open'])/report_semanal['Open']*100
-    report_semanal=report_semanal[['Tickers', 'Open', 'High', 'Low', 'Close', 'Resultado']]
+    report_semanal['Resultado %']=(report_semanal['Close'] - report_semanal['Open'])/report_semanal['Open']*100
+    report_semanal=report_semanal[['Tickers', 'Open', 'High', 'Low', 'Close', 'Resultado %']]
     report_semanal.rename(columns={'Tickers':'Commodities'}, inplace=True)
-    report_semanal=report_semanal.sort_values('Resultado', ascending=True)
-    download_report=report_semanal
+    report_semanal=report_semanal.sort_values('Resultado %', ascending=True)
+
     if tabela_botao == 'Compacta':
         st.dataframe(report_semanal)
         st.divider()
@@ -128,9 +129,6 @@ with tab2:
     weekly_returns = data.pct_change(periods=1) * 100
     sorted_returns = weekly_returns.iloc[-1].sort_values(ascending=False)
     sorted_indices = sorted_returns
-    # weekly_returns.fillna('teste')
-    # sorted_indices.fillna('teste')
-    # sorted_returns.fillna('teste')
 
     positive_returns = sorted_returns[sorted_returns >= 0]
     negative_returns = sorted_returns[sorted_returns < 0]
@@ -139,21 +137,19 @@ with tab2:
     ax.barh([dif_percentual[idx] for idx in positive_returns.index], positive_returns, color='green')
     ax.barh([dif_percentual[idx] for idx in negative_returns.index], negative_returns, color='red')
     ax.axvline(x=0, color='white', linestyle='--')
-    plt.xlabel('Variação Percetual')
-    plt.ylabel('Commodities')
+    # plt.xlabel('Variação Percetual')
+    # plt.ylabel('Commodities')
     fig.set_figwidth(15)
     ax.set_facecolor('#0e1117')
     fig.set_facecolor('#0e1117')
     ax.tick_params(colors='white')
     ax.xaxis.label.set_color('white')
     ax.yaxis.label.set_color('white')
-    ax.spines['bottom'].set_color('w')
-    ax.spines['top'].set_color('w')
-    ax.spines['left'].set_color('w')
-    ax.spines['right'].set_color('w')
-    # fig.set_figheight(30)
+    ax.spines['bottom'].set_color('#0e1117')
+    ax.spines['top'].set_color('#0e1117')
+    ax.spines['left'].set_color('#0e1117')
+    ax.spines['right'].set_color('#0e1117')
 
-    # Adiciona as porcentagens ao lado de cada barra
     for i, (index, value) in enumerate(zip(positive_returns.index, positive_returns)):
         ax.text(0, i, f'{value:.2f}%', ha='right', va='center', color='white', fontweight='bold')
         y = i
@@ -161,10 +157,13 @@ with tab2:
         ax.text(0, y+1+i, f'{value:.2f}%', ha='left', va='center', color='white', fontweight='bold')
 
     st.pyplot(plt.show())
+    st.write('')
+    st.write('')
 
     with st.expander("Ver explicação"):
-        st.write("O gráfico acima mostra a variação percentual das :orange[COMMODITIES] de acordo com a semana escolhida.")
+        st.write("O gráfico acima mostra a variação percentual das :orange[COMMODITIES] de acordo com a semana escolhida, que também pode ser visualizado pela tabela!")
     
+    download_report=report_semanal
     st.download_button("Baixar Tabela", 
                         download_report.to_csv(),
                         file_name='commodities_table_report.csv',
@@ -172,10 +171,15 @@ with tab2:
     
 with tab3:
     st.header("CORRELAÇÃO")
-
+    botao_corr=st.radio('Deseja ver com :blue[ATIVOS] juntos?',('Sim','Não'), index=1)
+    resultado_juncao=pd.concat([r_pd_commodities_tudo, r_pd_ativos], axis='columns')
+    resultado_juncao.index=resultado_juncao.index.date
     #       mostrando o dataframe da correlação e colocando heatmap
     corr_commodities_tudo=r_pd_commodities_tudo.corr()
-    download_all=r_pd_commodities_tudo.corr()
+    if botao_corr == 'Sim':
+        corr_commodities_tudo=resultado_juncao.corr()
+    else:
+        st.empty()
     if heatmap_botao == 'Ligado':
         if tabela_botao == 'Grande':
             cmap=plt.cm.get_cmap('RdYlGn')
@@ -190,8 +194,9 @@ with tab3:
             st.table(r_pd_commodities_tudo.corr())
 
     with st.expander("Ver explicação"):
-        st.write("O DataFrame acima mostra a correlação das :orange[COMMODITIES].")
+        st.write("A tabela acima mostra a correlação das :orange[COMMODITIES] com :blue[ATIVOS], caso a opção tenha sido marcada.")
 
+    download_all=corr_commodities_tudo
     st.download_button("Baixar Tabela", 
                        download_all.to_csv(),
                        file_name='commodities_table.csv',
@@ -199,17 +204,44 @@ with tab3:
 
 with tab4:
     st.header("CORRELAÇÃO SELECIONADA")
-    st.write("Selecione pelo menos 2 :orange[COMMODITIES] para correlação!")
-    st.text("")
+    st.write("Selecione :orange[COMMODITIES] e/ou :blue[ATIVOS] para a correlação! ")
+    ativos_corr=st.radio("Deseja adicionar ativos para correlação?", ('Sim', 'Não'), index=1)
     #   fazendo o multiselect
-    todas_colunas = r_pd_commodities_tudo2.columns.tolist()
+    todas_colunas = r_pd_commodities_tudo.columns.tolist()
     options_key = "_".join(todas_colunas)
-    colunas_selecionadas = st.multiselect('', options=todas_colunas)
-    
-    if colunas_selecionadas:
-        # r_pd_commodities_tudo2[colunas_selecionadas]
-        colunas_corr = r_pd_commodities_tudo2[colunas_selecionadas]
+    comm_selecionadas = st.multiselect('Selecione as :orange[COMMODITIES]:', options=todas_colunas)
+    todas_colunas_ativos=r_pd_ativos.columns.tolist()
+    options_key = "_".join(todas_colunas_ativos)
+    tudo=comm_selecionadas
+    download_selection=r_pd_commodities_tudo[comm_selecionadas].corr()
+    ativos_selecionados=comm_selecionadas
+    if ativos_corr == 'Sim':
+        ativos_selecionados = st.multiselect('Selecione os :blue[ATIVOS]:', options=todas_colunas_ativos)
+        tudo=(comm_selecionadas+ativos_selecionados)
+        download_selection=resultado_juncao[tudo].corr()
+    else:  
+        st.empty()
+
+    if comm_selecionadas:
+        colunas_corr = resultado_juncao[tudo]
         color_change_colunas_corr=colunas_corr.corr()
+        download_selection=resultado_juncao[tudo].corr()
+        if heatmap_botao == 'Ligado':
+            if tabela_botao == 'Grande':
+                cmap=plt.cm.get_cmap('RdYlGn')
+                st.table(color_change_colunas_corr.style.background_gradient(cmap=cmap,vmin=(-1),vmax=1, axis=None))
+            else:
+                cmap=plt.cm.get_cmap('RdYlGn')
+                st.dataframe(color_change_colunas_corr.style.background_gradient(cmap=cmap,vmin=(-1),vmax=1, axis=None))
+        else:
+            if tabela_botao == 'Compacta':
+                st.dataframe(color_change_colunas_corr)
+            else:
+                st.table(color_change_colunas_corr)
+    elif ativos_selecionados:
+        colunas_corr = resultado_juncao[tudo]
+        color_change_colunas_corr=colunas_corr.corr()
+        download_selection=resultado_juncao[tudo].corr()
         if heatmap_botao == 'Ligado':
             if tabela_botao == 'Grande':
                 cmap=plt.cm.get_cmap('RdYlGn')
@@ -223,9 +255,11 @@ with tab4:
             else:
                 st.table(color_change_colunas_corr)
     else:
-        st.write('')
+        st.empty()
+
+    with st.expander("Ver explicação"):
+        st.write("A tabela acima mostra a correlação das :orange[COMMODITIES] selecionadas entre si, ou com os :blue[ATIVOS] selecionados, caso a opção tenha sido marcada!")
     
-    download_selection=r_pd_commodities_tudo2[colunas_selecionadas].corr()
     st.download_button("Baixar Tabela", 
                        download_selection.to_csv(),
                        file_name='commodities_table_selection.csv',
